77566f2eb0255703438a30c5f56d834663162892
VARUN REDDY <varunreddy@VARUNs-MacBook-Pro-52.local>
2024-10-26 18:48:53 +0530

    image upload functionality Changes

--- client/src/components/admin-view/image-upload.jsx
+++ client/src/components/admin-view/image-upload.jsx
@@ -14,6 +14,8 @@ function ProductImageUpload({
     uploadedImageUrl,
     setUploadedImageUrl,
     setImageLoadingState,
+    isEditMode,
+
 }) {
   const inputRef = useRef(null);
 
@@ -73,7 +75,9 @@ function ProductImageUpload({
       <div
         onDragOver={handleDragOver}
         onDrop={handleDrop}
-        className={` border-2 border-dashed rounded-lg p-4`}
+        className={`${
+          isEditMode ? "opacity-60" : ""
+        } border-2 border-dashed rounded-lg p-4`}
       >
         <Input
           id="image-upload"
@@ -81,11 +85,14 @@ function ProductImageUpload({
           className="hidden"
           ref={inputRef}
           onChange={handleImageFileChange}
+          disabled={isEditMode}
         />
         {!imageFile ? (
           <Label
             htmlFor="image-upload"
-            className={` flex flex-col items-center justify-center h-32 cursor-pointer`}
+            className={`${
+              isEditMode ? "cursor-not-allowed" : ""
+            } flex flex-col items-center justify-center h-32 cursor-pointer`}
           >
             <UploadCloudIcon className="w-10 h-10 text-muted-foreground mb-2" />
             <span>Drag & drop or click to upload image</span>
@@ -120,6 +127,7 @@ ProductImageUpload.propTypes = {
   setUploadedImageUrl: PropTypes.func,
   setImageLoadingState: PropTypes.func,
   imageLoadingState: PropTypes.bool,
+  isEditMode: PropTypes.bool,
 };
 
 export default ProductImageUpload;
